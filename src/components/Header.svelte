d8380f5e765976ce67298e0692eabf3c21f38226
Siven Panda <siven.panda@duke.edu>
2025-02-28 03:58:06 -0500

    first

---
+++ src/components/Header.svelte
@@ -0,0 +1,66 @@
+<script>
+</script>
+
+<div class="wrap">
+    <a href="/"><img src={'/lari.svg'} width="115" height="48" alt="Lari Homepage" /></a>
+    <div class="actions">
+        <a href="/contact">Contact</a>
+        <a href="/about">About</a>
+        <a class="bt-log" href="/login">Log in</a>
+    </div>
+</div>
+    
+
+<style>
+    .wrap {
+        --padding-left-right: 3vw;
+        background-color: rgba(124, 124, 124, 0.8);
+        backdrop-filter: blur(20px);
+        height: 7vh;
+        width: calc(100vw - 2 * var(--padding-left-right));
+        position: fixed;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        top: 0;
+        padding-left: var(--padding-left-right);
+        padding-right: var(--padding-left-right);
+        overflow: hidden;
+    }
+
+    .actions {
+        display: flex;
+        flex-direction: row;
+        justify-content: right;
+        gap: 3em;
+        width: 50vw;
+        align-items: center;
+    }
+
+    .actions > a {
+        text-decoration: none;
+        color: white;
+        border-radius: 5px;
+    }
+
+    .actions > a:hover {
+        color: rgb(201, 201, 201);
+        transition: 200ms ease-in-out;
+    }
+
+    .bt-log {
+        background-color: rgb(76, 81, 158);
+        text-decoration: none;
+        color: white;
+        padding: .5em;
+        border-radius: 5px;
+        transition: 300ms ease-in-out;
+    }
+
+    .bt-log:hover {
+        background-color: rgb(74, 71, 130);
+        color: rgb(201, 201, 201);
+        transition: 200ms ease-in-out;
+    }
+</style>
---
+++ src/components/Home.svelte
@@ -0,0 +1,54 @@
+<div class="wrap">
+    <div class="hero">
+        <h1>Managing health appointments doesn't have to be difficult.</h1>
+        <p>We make it easy to manage your appointments so you can focus on what matters.</p>
+        <a class="bt" href="#how">Let's go</a>
+    </div>
+    <div id="how">
+        <h1>How do we do it?</h1>
+    </div>
+</div>
+
+<style>
+    .wrap {
+        height: 100vh;
+        width: 100vw;
+        overflow-y: scroll;
+        overflow-x: hidden;
+    }
+
+    .hero {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        color: white;
+        flex-direction: column;
+        height: 100vh;
+        width: 100vw;
+        background: linear-gradient(0deg, rgb(31, 31, 31) 00%, rgba(31, 31, 31, .4) 70%, rgba(0, 0, 0, 0.7)),
+                url('/hallway.jpg');
+        background-size: cover;
+    }
+
+    .hero > h1 {
+        padding: 0;
+        margin: 0;
+    }
+
+    .bt {
+        background-color: rgb(76, 81, 158);
+        text-decoration: none;
+        color: white;
+        padding: .5em;
+        border-radius: 5px;
+        transition: 300ms ease-in-out;
+    }
+
+    .bt:hover {
+        background-color: rgb(74, 71, 130);
+        color: rgb(201, 201, 201);
+        transition: 200ms ease-in-out;
+    }
+
+</style>
